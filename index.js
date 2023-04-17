cb531c6cd2f98fccb0e5c08b2acbc6f59bbe6cc4
Bonds95 <sergeybondarenko95@yandex.ru>
2023-04-17 09:43:31 +0300

    hw done

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 function seasons(month) {
     if (month > 0 && month < 3 || month == 12) {
-        console.log('Зима')
+        alert('Зима')
 
     }
     else if (isNaN(month)) {
-        console.log('Вы ввели неверное значение')
+        alert('Вы ввели неверное значение')
     }
     else if (month > 2 && month < 6) {
-        console.log('Весна')
+        alert('Весна')
     }
     else if (month > 5 && month < 9) {
-        console.log('Весна')
+        alert('Лето')
     }
     else if (month > 8 && month < 12) {
-        console.log('Весна')
+        alert('Осень')
     }
     else {
-        console.log('Такого месяца не существует. В году 12 месяцев')
+        alert('Такого месяца не существует. В году 12 месяцев')
 
     }
 }
 
-seasons(3)
+seasons(prompt('Напишите номер месяца'))
+
